c231e9fdaa6d789b660fd7a8799a77b1e85e042d
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-07-16 09:33:09 +0900

    🎉 :: Project Start

---
+++ Team_ggamsi/Team_ggamsi/Team_ggamsiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team_ggamsiApp.swift
+//  Team_ggamsi
+//
+//  Created by bibiga on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Team_ggamsiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
